b22399c240381e6ad6e212fcb72e5316fe5f37ca
fjbalsamo <fjbalsamo@gmail.com>
2021-06-25 22:12:34 -0300

    emit events from a components

---
+++ src/components/Hijo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Hola Soy Hijo</p>
+    <button type="button" @click="onClick()">Hijo Click</button>
+    <hr />
+    <button type="button" @click="emitValue()">Emit value</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hijo",
+  data() {
+    return {
+      nombre: "",
+    };
+  },
+  methods: {
+    saludar() {
+      alert("hola soy Hijo");
+    },
+    onClick() {
+      this.$emit("click");
+    },
+    emitValue() {
+      this.$emit("value", 2);
+    },
+  },
+};
+</script>
---
+++ src/components/Padre.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Hola Soy Padre</h2>
+    <Hijo ref="hijoRef" v-on:click="contar" v-on:value="catchValue" />
+    <hr />
+    <button type="button" @click="saludarHijo()">saludar hijo</button>
+    <hr />
+    <h3>{{ contador }}</h3>
+  </div>
+</template>
+
+<script>
+import Hijo from "./Hijo.vue";
+
+export default {
+  name: "Padre",
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  components: {
+    Hijo,
+  },
+  methods: {
+    saludarHijo() {
+      this.$refs["hijoRef"].saludar();
+    },
+    contar(valor) {
+      if (valor) {
+        console.log(valor);
+      }
+      this.contador++;
+    },
+    catchValue(value) {
+      //alert(value);
+      console.log(value + 1);
+    },
+  },
+};
+</script>
